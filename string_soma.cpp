832ce8128d4b0f52d08ec16f549c4688ed74d823
Gabriel Vasconcelos <gabrielvasconcelos@cjr.org.br>
2017-09-09 21:15:07 -0300

    Teste \n no final e delimitador eh a virgula

--- string_soma.cpp
+++ string_soma.cpp
@@ -1,10 +1,24 @@
 #include <stdio.h>
 #include <string.h>
+#include <stack>
+
+int ehNumero(char ch) {
+	if (ch == '0' || ch == '1' || ch == '2' || ch == '3' || ch == '4' || ch == '5' || ch == '6' || ch == '7' || ch == '8' || ch == '9')
+		return 1;
+	return 0;
+}
 
 int soma_string(const char * string_entrada) {
+	std::stack<char> pilhaDelimitadores;
+	char delimitador = ',';
 	int n = strlen(string_entrada);
+
 	if (string_entrada[n-1] != '\n') {
 		return -1;
+	}
+	for (int i = 0; i < n; i++)	{
+		char ch = string_entrada[i];
+		if (!ehNumero(ch) && ch != delimitador && ch != '\n') return -1;
 	}
 
 	return 0;
--- testa_string_soma.cpp
+++ testa_string_soma.cpp
@@ -3,9 +3,15 @@
 
 TEST_CASE("Calculadora simples de soma", "[calculadora][soma]") {
 	SECTION("Formatacao da entrada") {
-		SECTION("\n no final"){
-			CHECK(soma_string("") == -1);
-			CHECK(soma_string("\n") != -1);
+		SECTION("\\n no final"){
+			REQUIRE(soma_string("") == -1);
+			REQUIRE(soma_string("\n") != -1);
 		}
+		SECTION("Delimitador é a ','"){
+			REQUIRE(soma_string("1 1\n") == -1);
+			REQUIRE(soma_string("1,1\n") != -1);
+		}
+	}
+	SECTION("Calculo da soma"){
 	}
 }
