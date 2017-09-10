158416312ba1ff23c9ebc1329603f56fbb263d85
Gabriel Vasconcelos <gabrielvasconcelos@cjr.org.br>
2017-09-10 16:32:37 -0300

    Teste de numero antes e depois de todo delimitador

--- string_soma.cpp
+++ string_soma.cpp
@@ -2,16 +2,16 @@
 #include <string.h>
 #include <stack>
 
-int ehNumero(char ch) {
+bool ehNumero(char ch) {
 	if (ch == '0' || ch == '1' || ch == '2' || ch == '3' || ch == '4' || ch == '5' || ch == '6' || ch == '7' || ch == '8' || ch == '9')
-		return 1;
-	return 0;
+		return true;
+	return false;
 }
 bool validaAlgarismosLinha(const char * palavra, int n) {
 	int count;
 	for (int i = 0; i < n; i++) {
 		count = 0;
-		while (palavra[i] != '\n'){
+		while (palavra[i] != '\n') {
 			if (ehNumero(palavra[i])) count++;
 			i++;
 		}
@@ -19,6 +19,33 @@ bool validaAlgarismosLinha(const char * palavra, int n) {
 	}
 	return true;
 }
+int contaNumeros(const char * palavra, int n){
+	int j = 1;
+	int count = 0;
+	for (int i = 0; i < n; i++) {
+		if (ehNumero(palavra[i])) {
+			while (ehNumero(palavra[i+j])) {
+				i++;
+			}
+			count++;
+		}
+	}
+	return count;
+}
+int contaDelimitadores(const char * palavra, int n, char delimitador) {
+	int count = 0;
+	for (int i = 0; i < n; i++) {
+		if (palavra[i] == delimitador) count++;
+	}
+	return count;
+}
+bool validaDelimitador(const char * palavra, int n, char delimitador) {
+	bool vazia = contaDelimitadores(palavra, n, delimitador) + contaNumeros(palavra, n) == 0;
+	bool numDelimitadorValido = contaDelimitadores(palavra, n, delimitador) + 1 == contaNumeros(palavra, n);
+	if (vazia || numDelimitadorValido)	return true;
+	return false;
+}
+
 
 int soma_string(const char * string_entrada) {
 	char delimitador = ',';
@@ -26,11 +53,12 @@ int soma_string(const char * string_entrada) {
 	int tamanho = strlen(string_entrada);
 
 	if (string_entrada[tamanho-1] != '\n') return -1; //enter no final
-	for (int i = 0; i < tamanho; i++)	{
+	for (int i = 0; i < tamanho; i++) {
 		ch = string_entrada[i];
 		if (!ehNumero(ch) && ch != delimitador && ch != '\n') return -1;
 	}
-	if(!validaAlgarismosLinha(string_entrada, tamanho)) return -1;
-
+	if (!validaAlgarismosLinha(string_entrada, tamanho)) return -1;
+	if (!validaDelimitador(string_entrada, tamanho, delimitador)) return -1;
+	
 	return 0;
 }
--- testa_string_soma.cpp
+++ testa_string_soma.cpp
@@ -15,9 +15,21 @@ TEST_CASE("Calculadora simples de soma", "[calculadora][soma]") {
 		SECTION("Tem de 0 a 3 numeros por linha") {
 			REQUIRE(soma_string("1,1,1,1\n") == -1);
 			REQUIRE(soma_string("1,1,1,1,1\n") == -1);
+			REQUIRE(soma_string("\n1,1,1,1\n") == -1);
 			REQUIRE(soma_string("1\n") != -1);
-			REQUIRE(soma_string("1,1\n") != -1);
 			REQUIRE(soma_string("1,1,1\n") != -1);
+			REQUIRE(soma_string("\n1,1\n") != -1);
+			REQUIRE(soma_string("\n\n1,1\n") != -1);
+		}
+		SECTION("Numero antes de todo delimitador") {
+			REQUIRE(soma_string("1,\n") == -1);
+			REQUIRE(soma_string(",1\n") == -1);
+			REQUIRE(soma_string("1,,1\n") == -1);
+			REQUIRE(soma_string(",\n") == -1);
+			SECTION("\\n sao ignorados na contagem") {
+				REQUIRE(soma_string("1\n\n\n,1\n") != -1);
+				REQUIRE(soma_string("1\n\n\n,\n\n\n1\n") != -1);
+			}
 		}
 	}
 }
